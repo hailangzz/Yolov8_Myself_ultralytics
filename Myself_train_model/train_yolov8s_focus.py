56fed5bc9012f94c9e0b1849b1a75f8c4dd2af0f
UltralyticsAssistant <web@ultralytics.com>
2026-04-24 04:29:15 +0000

    Auto-format by https://ultralytics.com/actions

--- Myself_train_model/train_yolov8s_focus.py
+++ Myself_train_model/train_yolov8s_focus.py
@@ -9,14 +9,17 @@ from ultralytics import YOLO
 
 
 # 训练线材检测模型
-model = YOLO("/home/chenkejing/PycharmProjects/ultralytics/ultralytics/cfg/models/v8/yolov8s_focus_wire.yaml")  # load a pretrained model (recommended for training)
+model = YOLO(
+    "/home/chenkejing/PycharmProjects/ultralytics/ultralytics/cfg/models/v8/yolov8s_focus_wire.yaml"
+)  # load a pretrained model (recommended for training)
 model.load("/home/chenkejing/PycharmProjects/ultralytics/yolov8s.pt")
-results = model.train(data="wire_detect.yaml", epochs=100, imgsz=640, device=0, workers=0, resume=True, project="runs/my_wire_exp", name="yolov8_focus_v1")
-
-
-
-
-
-
-
-
+results = model.train(
+    data="wire_detect.yaml",
+    epochs=100,
+    imgsz=640,
+    device=0,
+    workers=0,
+    resume=True,
+    project="runs/my_wire_exp",
+    name="yolov8_focus_v1",
+)
