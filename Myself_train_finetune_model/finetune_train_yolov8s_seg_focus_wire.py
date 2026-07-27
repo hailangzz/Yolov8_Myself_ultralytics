b5887e3463b60ac206c33fe635def7952669ab28
UltralyticsAssistant <web@ultralytics.com>
2026-07-27 04:14:22 +0000

    Auto-format by https://ultralytics.com/actions

--- Myself_train_finetune_model/finetune_train_yolov8s_seg_focus_wire.py
+++ Myself_train_finetune_model/finetune_train_yolov8s_seg_focus_wire.py
@@ -1,7 +1,7 @@
 """
 
 # ✅ Stage 1：只训练 head（稳定收敛）
-from ultralytics import YOLO
+from ultralytics import YOLO.
 
 if __name__ == "__main__":
 
@@ -38,54 +38,37 @@ if __name__ == "__main__":
 
 """
 
-
 # ✅ Stage 2：全量 finetune（核心提升阶段）
 from ultralytics import YOLO
 
 if __name__ == "__main__":
-
     model = YOLO("runs/yolov8s_wire_seg_finetune_stage1/stage1_head_only/weights/best.pt")
 
     model.train(
         task="segment",
         data="finetune_coco8-seg_wire.yaml",
-
         # epochs=80,
         epochs=150,
         imgsz=960,
-        batch=8,                # ⭐ 稳一点
-
+        batch=8,  # ⭐ 稳一点
         device=0,
         workers=4,
-
         optimizer="SGD",
-        lr0=3e-4,               # ⭐ 比三阶段稍高一点
+        lr0=3e-4,  # ⭐ 比三阶段稍高一点
         cos_lr=True,
-
-        freeze=0,               # ⭐ 全解冻
-
+        freeze=0,  # ⭐ 全解冻
         mosaic=0.2,
         mixup=0.0,
         copy_paste=0.0,
-
         hsv_h=0.01,
         hsv_s=0.4,
         hsv_v=0.3,
-
         translate=0.05,
         scale=0.3,
         fliplr=0.5,
-
         weight_decay=0.0005,
         amp=False,
-
         patience=40,
-
         project="runs/yolov8s_wire_seg_finetune_stage1",
-        name="stage2_full_finetune"
+        name="stage2_full_finetune",
     )
-
-
-
-
-
--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -8,7 +8,7 @@ from copy import deepcopy
 from pathlib import Path
 
 import torch
-import torch.nn as nn
+from torch import nn
 
 from ultralytics.nn.autobackend import check_class_names
 from ultralytics.nn.modules import (
@@ -30,6 +30,7 @@ from ultralytics.nn.modules import (
     Bottleneck,
     BottleneckCSP,
     C2f,
+    C2f_SA,
     C2fAttn,
     C2fCIB,
     C2fPSA,
@@ -46,6 +47,7 @@ from ultralytics.nn.modules import (
     Detect,
     DWConv,
     DWConvTranspose2d,
+    EdgeConv,
     Focus,
     GhostBottleneck,
     GhostConv,
@@ -68,8 +70,6 @@ from ultralytics.nn.modules import (
     YOLOEDetect,
     YOLOESegment,
     v10Detect,
-    EdgeConv,
-    C2f_SA,
 )
 from ultralytics.utils import DEFAULT_CFG_DICT, LOGGER, YAML, colorstr, emojis
 from ultralytics.utils.checks import check_requirements, check_suffix, check_yaml
@@ -1331,11 +1331,9 @@ class SafeClass:
 
     def __init__(self, *args, **kwargs):
         """Initialize SafeClass instance, ignoring all arguments."""
-        pass
 
     def __call__(self, *args, **kwargs):
         """Run SafeClass instance, ignoring all arguments."""
-        pass
 
 
 class SafeUnpickler(pickle.Unpickler):
@@ -1557,7 +1555,6 @@ def parse_model(d, ch, verbose=True):
             A2C2f,
             EdgeConv,
             C2f_SA,
-
         }
     )
     repeat_modules = frozenset(  # modules with 'repeat' arguments
